9da70e8aca4e8baa3b7e5cd8f16f34755d36bed1
Hope <hopewcurtis@gmail.com>
2025-03-25 16:21:01 -0400

    stopping at numbers

--- formatted strings.app.py
+++ formatted strings.app.py
@@ -2,3 +2,4 @@ first = "Mosh"
 last = "Hamedani"
 full = f"{len(first)} {2+2} "
 print(full)
+# format a string you can put any valid expression in between curly braces
--- string methods app.py
+++ string methods app.py
@@ -1,1 +1,13 @@
-course = "Python Programming"
+course = "     python programming    "
+print(course.upper())  # all letters in upper case
+print(course)
+print(course.lower())   # sll letters in lower case
+print(course.title())  # first two letters capitalized
+# removes the white space ...also  r and l gets rid of white space on left and right
+print(course.strip())
+# index of character or seq of character  "calling the find method" returns the index
+print(course.find("Pro"))
+print(course.replace("p", "j"))  # replace a letter or word
+# existance of charater this the "in" operator "checking the "expression" returns the boulon of true or false
+print("pro" in course)
+print("swift"not in course)  # does not contain characters or seq of characters
