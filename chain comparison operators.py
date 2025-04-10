ad0e3cdae93b34ff54a0e0fb36e95e8b2b7cd5ba
hwcurtis <hopewcurtis@gmail.com>
2025-04-10 17:00:55 -0400

    pravtice

---
+++ chain comparison operators.py
@@ -0,0 +1,7 @@
+# chaining comparison operator
+
+# age should be betweeen 16 and 6t5
+
+age = 22
+if 18 < + age < 65:
+    print("Eligible")
---
+++ code with mosh/for else.py
@@ -0,0 +1,6 @@
+successful = True
+for number in range(3):
+    print("attempt")
+    if successful:
+        print("successful")
+        break
