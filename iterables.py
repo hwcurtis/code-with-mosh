0134e8bd5b69442138cea73256672882c2365a6a
hwcurtis <hopewcurtis@gmail.com>
2025-04-12 17:42:14 -0400

    hopefully fixed

---
+++ iterables.py
@@ -0,0 +1,5 @@
+# print(type(5))
+# print(type(range(5)))# iterable you can  iterate over it and use it in a for  loop
+
+for x in [1, 2, 3, 4]:  # range x is a different value   strings are also iterable  x holds 1 character
+    print(x)
